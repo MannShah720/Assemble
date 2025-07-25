70cf7518bb62028e55f93b25950168c1f5501883
MannShah720 <shahmann0108@gmail.com>
2025-07-25 22:19:42 +0100

    added game status, language chips, word section, alphabet keyboard buttons & new game button

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,65 @@
 import { useState } from 'react'
 import './App.css'
+import { languages } from "./languages.js"
 
 function App() {
+
+  const [currentWord, setCurrentWord] = useState("react")
+
+  // Keyboard
+  const alphabet = "abcdefghijklmnopqrstuvwxyz"
+  const keyboardElements = alphabet.split("").map(letter => {
+    return (
+      <button key={letter}>{letter.toUpperCase()}</button>
+    )
+  })
+
+  const wordElements = currentWord.split("").map((letter, index) => {
+    return (
+      <span key={index}>{letter.toUpperCase()}</span>
+    )
+  })
+
+  const langElements = languages.map(lang => {
+    const styles = {
+      backgroundColor: lang.backgroundColor,
+      color: lang.color
+    }
+
+    return (
+      <span 
+      key={lang.name}
+      className="chip" 
+      style={styles}>{lang.name}</span>
+    )
+  })
 
   return (
     <main>
       <header>
-          <h1>Assembly: Endgame</h1>
-          <p>Guess the word within 8 attempts to keep the 
-          programming world safe from Assembly!</p>
+          <h1>Codel</h1>
+          <p>Guess the word within 8 attempts to protect the 
+          programming languages!</p>
       </header>
+
+      <section className="game-status">
+        <h2>You Win!</h2>
+        <p>Well done! 🎉</p>
+      </section>
+
+      <section className="language-chips">
+        {langElements}
+      </section>
+
+      <section className="word">
+        {wordElements}
+      </section>
+
+      <section className="keyboard">
+        {keyboardElements}
+      </section>
+
+      <button className="new-game">New Game</button>
     </main>
   )
 }
---
+++ src/languages.js
@@ -0,0 +1,47 @@
+export const languages = [
+    {
+        name: "Python",
+        backgroundColor: "#FFD742",
+        color: "#1E1E1E",
+    },
+    {
+        name: "Bash",
+        backgroundColor: "#328AF1",
+        color: "#F9F4DA",
+    },
+    {
+        name: "JavaScript",
+        backgroundColor: "#F4EB13",
+        color: "#1E1E1E",
+    },
+    {
+        name: "C++",
+        backgroundColor: "#2ED3E9",
+        color: "#1E1E1E",
+    },
+    {
+        name: "TypeScript",
+        backgroundColor: "#298EC6",
+        color: "#F9F4DA",
+    },
+    {
+        name: "Java",
+        backgroundColor: "#599137",
+        color: "#F9F4DA",
+    },
+    {
+        name: "SQL",
+        backgroundColor: "#ec7d15ff",
+        color: "#F9F4DA",
+    },
+    {
+        name: "Ruby",
+        backgroundColor: "#D02B2B",
+        color: "#F9F4DA",
+    },
+    {
+        name: "Assembly",
+        backgroundColor: "#2D519F",
+        color: "#F9F4DA",
+    },
+]
